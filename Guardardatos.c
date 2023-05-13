1bde7df9e093c9980be8e349606a442201f2697f
Jtaas <130755224+Jtaas@users.noreply.github.com>
2023-05-13 13:36:29 +0200

    Vector de estructuras
    He copiado el programa de cecilia y he añadido la idea para el vector de estructuras

---
+++ Guardardatos.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+typedef struct 
+{
+	int month, year;
+	float gwh;
+} energia;
+
+
+int main()
+{
+	FILE *plectura;
+	plectura = fopen("tw_E10X_2022_2023-rayomcqueen95/generacion_por_tecnologias_21_22_puntos.csv", "r");
+	
+	if (plectura == NULL)
+	{
+		printf("Error al abrir el fichero.\n");
+		return -1;
+	}
+	else
+	{
+		printf("Fichero abierto correctamente.\n");
+		
+		char c;
+		while(fscanf(plectura, "%c", &c) != EOF)
+		{
+			//no se como leer el excel
+		}
+		
+		//El vector de estructuras "fuente" guardará los datos de las fuentes energéticas leídas en el fichero.	
+		energia fuente[40];
+		int nfuentes=0;	
+		int i;
+		
+		for(i=0; i<nfuentes; i++)
+		{
+			fuente[i].month =
+		}
+		
+		
+		return 0;
+	}
+}
+
+
+
+
