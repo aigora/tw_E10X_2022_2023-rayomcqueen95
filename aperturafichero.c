ca436a2a8e5f939c975c6359044560d155fe1c6f
ceciliabp <130695845+ceciliabp@users.noreply.github.com>
2023-05-16 21:53:03 +0200

    cambié el nombre del archivo

--- aperturafichero.c
+++ aperturafichero.c
@@ -3,8 +3,8 @@
 
 int main(){
 	FILE *plectura;
-	// AtenciÛn a los separadores en la ruta del fichero, y a las comillas dobles
-	plectura = fopen("C:/Users/34698/Desktop/COSAS UNI/INFORM¡TICA/TRABAJO/generacion_por_tecnologias_21_22_puntos.csv", "r");
+	// Atenci√≥n a los separadores en la ruta del fichero, y a las comillas dobles
+	plectura = fopen("C:/Users/34698/Desktop/COSAS UNI/INFORM√ÅTICA/TRABAJO/generacion_por_tecnologias_21_22_puntos.csv", "r");
 	if (plectura == NULL)
 	{
 		printf("Error al abrir el fichero.\n");
